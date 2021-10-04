364dd65823b395d70a9fe8e94ceca90496320e4b
limhyoseok <nonamedoud@gmail.com>
2021-10-04 03:02:26 +0000

    chore : delete preprocessing_dataset func

--- load_data.py
+++ load_data.py
@@ -18,18 +18,18 @@ class RE_Dataset(torch.utils.data.Dataset):
   def __len__(self):
     return len(self.labels)
 
-def preprocessing_dataset(dataset):
-  """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
-  subject_entity = []
-  object_entity = []
-  for i,j in zip(dataset['subject_entity'], dataset['object_entity']):
-    i = i[1:-1].split(',')[0].split(':')[1]
-    j = j[1:-1].split(',')[0].split(':')[1]
+# def preprocessing_dataset(dataset):
+#   """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
+#   subject_entity = []
+#   object_entity = []
+#   for i,j in zip(dataset['subject_entity'], dataset['object_entity']):
+#     i = i[1:-1].split(',')[0].split(':')[1]
+#     j = j[1:-1].split(',')[0].split(':')[1]
 
-    subject_entity.append(i)
-    object_entity.append(j)
-  out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
-  return out_dataset
+#     subject_entity.append(i)
+#     object_entity.append(j)
+#   out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
+#   return out_dataset
 
 def load_data(dataset_dir):
   """ csv 파일을 경로에 맡게 불러 옵니다. """
