024d603dc1018cccd23ddca2ee64a1dc75324377
limhyoseok <nonamedoud@gmail.com>
2021-10-04 03:05:24 +0000

    chore : delete prepro~ and using dict

--- load_data.py
+++ load_data.py
@@ -18,33 +18,26 @@ class RE_Dataset(torch.utils.data.Dataset):
   def __len__(self):
     return len(self.labels)
 
-# def preprocessing_dataset(dataset):
-#   """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
-#   subject_entity = []
-#   object_entity = []
-#   for i,j in zip(dataset['subject_entity'], dataset['object_entity']):
-#     i = i[1:-1].split(',')[0].split(':')[1]
-#     j = j[1:-1].split(',')[0].split(':')[1]
 
-#     subject_entity.append(i)
-#     object_entity.append(j)
-#   out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
-#   return out_dataset
 
 def load_data(dataset_dir):
   """ csv 파일을 경로에 맡게 불러 옵니다. """
   pd_dataset = pd.read_csv(dataset_dir)
-  dataset = preprocessing_dataset(pd_dataset)
+
+  # str to dict
+  pd_dataset["subject_entity"] = pd_dataset["subject_entity"].apply(lambda x : eval(x))
+  pd_dataset["object_entity"] = pd_dataset["object_entity"].apply(lambda x : eval(x))
   
-  return dataset
+  return pd_dataset
 
 def tokenized_dataset(dataset, tokenizer):
   """ tokenizer에 따라 sentence를 tokenizing 합니다."""
   concat_entity = []
   for e01, e02 in zip(dataset['subject_entity'], dataset['object_entity']):
     temp = ''
-    temp = e01 + '[SEP]' + e02
+    temp = e01["word"] + '[SEP]' + e02["word"]
     concat_entity.append(temp)
+  
   tokenized_sentences = tokenizer(
       concat_entity,
       list(dataset['sentence']),
